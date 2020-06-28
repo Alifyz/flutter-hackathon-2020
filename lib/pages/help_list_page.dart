23c89c3b4e42bee9fd3c2d8d999c0dcec5a58fc1
Alifyz <alifyzdev@gmail.com>
2020-06-27 21:24:57 -0300

    add: header for help list page

--- lib/pages/help_list_page.dart
+++ lib/pages/help_list_page.dart
@@ -1,30 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hackathon/styles/colors.dart';
+import 'package:flutter_hackathon/styles/text.dart';
 
-class HelpListPage extends StatelessWidget {
+class HelpListPage extends StatefulWidget {
+  @override
+  _HelpListPageState createState() => _HelpListPageState();
+}
+
+class _HelpListPageState extends State<HelpListPage> {
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    double width = MediaQuery.of(context).size.width;
-
     return Scaffold(
-      backgroundColor: ColorStyles.backgroundColor,
-      body: Stack(
-        children: [
-          Container(
-            color: Colors.black,
-            child: Image.asset(
-              'images/background.png',
-              height: height,
-              width: width,
-              fit: BoxFit.fill,
+        backgroundColor: ColorStyles.backgroundColor,
+        body: Padding(
+          padding: const EdgeInsets.only(top: 56),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'PEDIDOS DE SOCORRO',
+                  style: TypographyStyle.textTitleTheme,
+                ),
+              ],
             ),
           ),
-          Container(
-            color: Colors.black54,
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
